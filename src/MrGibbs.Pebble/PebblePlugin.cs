3489c2e1f996646842a48613bc62cc4f832be7d5
Brook Patten <brook.patten@gmail.com>
2016-01-30 15:14:24 -0500

    updated pebble plugin and viewer to match organizational changes to pebblesharp

--- src/MrGibbs.Pebble/PebblePlugin.cs
+++ src/MrGibbs.Pebble/PebblePlugin.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
-using PebbleSharp.Net45;
+using PebbleSharp.Mono.BlueZ5;
 using PebbleSharp.Core.NonPortable;
 using PebbleSharp.Core.Bundles;
 using MrGibbs.Contracts;
 using MrGibbs.Contracts.Infrastructure;
+using Mono.BlueZ.DBus;
 
 namespace MrGibbs.Pebble
 {
@@ -17,6 +18,7 @@ namespace MrGibbs.Pebble
         private ILogger _logger;
         private bool _initialized = false;
         private IList<IPluginComponent> _components;
+		private PebbleManager _manager;
 
 #if !WINDOWS
         private const string _pbwPath = "/home/pi/dev/mrgibbs/MrGibbs.Pebble/Mr._Gibbs.pbw";
@@ -26,9 +28,16 @@ namespace MrGibbs.Pebble
         private const string _pbwPath = "Mr._Gibbs.pbw";
 #endif
 
-        public PebblePlugin(ILogger logger)
+		public PebblePlugin(ILogger logger)
+		{
+			_logger = logger;
+			_manager = new PebbleManager ();
+		}
+
+        public PebblePlugin(ILogger logger,DBusConnection connection)
         {
             _logger = logger;
+			_manager = new PebbleManager (connection);
         }
 
         public void Initialize(PluginConfiguration configuration, Action<Action<ISystemController, IRaceController>> queueCommand)
@@ -36,7 +45,7 @@ namespace MrGibbs.Pebble
             _components = new List<IPluginComponent>();
         
             //scan for pebbles
-			var pebbles = PebbleNet45.Detect (null, false);
+			var pebbles = _manager.Detect (null, false);
 
             AppBundle bundle=null;
 
@@ -110,6 +119,7 @@ namespace MrGibbs.Pebble
 
         public void Dispose()
         {
+			_manager.Dispose ();
             if (_components != null)
             {
                 foreach (var component in _components)
