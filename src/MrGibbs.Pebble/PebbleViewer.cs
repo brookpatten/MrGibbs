888f26eb8e2a4620026c1e8388efd257783774eb
Brook Patten <brook.patten@gmail.com>
2016-03-11 23:20:18 -0500

    added coordinate calculations for #5 to create a simple course image from gps coordinates

--- src/MrGibbs.Pebble/PebbleViewer.cs
+++ src/MrGibbs.Pebble/PebbleViewer.cs
@@ -324,6 +324,80 @@ Course Relative*/
             }
         }
 
+		/// <summary>
+		/// Given projected gps points, converts to a simple set of coordinates that can be 
+		/// rendered as an image on the pebble
+		/// </summary>
+		/// <returns>The course.</returns>
+		/// <param name="windward">Windward.</param>
+		/// <param name="leeward">Leeward.</param>
+		/// <param name="boat">Boat.</param>
+		private void PrerenderCourse (Vector2 windward, Vector2 leeward, Vector2 boat)
+		{
+			//size of the pebble screen
+			Vector2 targetSize = new Vector2() { X = 144, Y = 168 };
+
+			//center everything on the origin by shifting it by the center of the course
+			Vector2 courseCenter = new Vector2()
+			{
+				X = (windward.X + leeward.X) / 2f,
+				Y = (windward.Y + leeward.Y) / 2f
+			};
+			windward.X = windward.X - courseCenter.X;
+			windward.Y = windward.Y - courseCenter.Y;
+			leeward.X = leeward.X - courseCenter.X;
+			leeward.Y = leeward.Y - courseCenter.Y;
+			boat.X = boat.X - courseCenter.X;
+			boat.Y = boat.Y - courseCenter.Y;
+
+			//rotate such that windward is at the top and leeward is at the bottom
+			var windwardPolar = windward.CartesianToPolar ();
+			var theta = windwardPolar.Theta - ((float)Math.PI / 2f);
+
+			var boatPolar = boat.CartesianToPolar();
+			boatPolar.Theta = boatPolar.Theta - theta;
+			boat = boatPolar.PolarToCartesian();
+
+			windwardPolar.Theta = windwardPolar.Theta - theta;
+			windward = windwardPolar.PolarToCartesian();
+
+			var leewardPolar = leeward.CartesianToPolar();
+			leewardPolar.Theta = leewardPolar.Theta - theta;
+			leeward = leewardPolar.PolarToCartesian();
+
+			//at this point the course is centered over the origin
+			//with the rhumb line on the y axis
+			//scale everything down to fit on the desired size
+			float currentSize = Math.Abs(windward.Y - leeward.Y);
+			float scaleFactor = targetSize.Y/ currentSize ;
+
+			boat.X = boat.X * scaleFactor;
+			boat.Y = boat.Y * scaleFactor;
+
+			windward.X = windward.X * scaleFactor;
+			windward.Y = windward.Y * scaleFactor;
+
+			leeward.X = leeward.X * scaleFactor;
+			leeward.Y = leeward.Y * scaleFactor;
+
+
+			//shift the whole thing into the TR quadrant and flip it over the x axis
+			//so it matches drawing coords
+			Vector2 shift = new Vector2()
+			{
+				X = targetSize.X / 2f,
+				Y = -(targetSize.Y / 2f) 
+			};
+			boat.X = boat.X + shift.X;
+			boat.Y = Math.Abs(boat.Y + shift.Y);
+			windward.X = windward.X + shift.X;
+			windward.Y = Math.Abs(windward.Y + shift.Y);
+			leeward.X = leeward.X + shift.X;
+			leeward.Y = Math.Abs(leeward.Y + shift.Y);
+
+			//return these.... or draw an image.... or something useful
+		}
+
         /// <inheritdoc />
         public IPlugin Plugin
         {
